722c30e64afde14042c468d39fb73b62462bf083
PY Liu <140623725+fraubentz@users.noreply.github.com>
2024-10-24 12:28:09 +0100

    Code grid-size prompt, eraser and rgb randomiser

--- script.js
+++ script.js
@@ -2,18 +2,45 @@ let container = document.querySelector(".container");
 
 
 function populateGrid(num) {
-    const squareSize = 700 / num;
+    const squareSize = 600 / num;
     for (let i = 0; i < (num*num); i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
         square.style.borderColor = "black";
-        square.style.borderWidth = "thick";
-        square.textContent = "Square";
-        console.log("square");
+        square.style.borderWidth = "0.1rem";
+        //square.textContent = "Square";
+        //console.log("square");
         container.appendChild(square);
     }
 }
+
+function getValue() {
+    //let inputField = document.querySelector(".valueSquares");
+    //let value = inputField.value;
+    let value = prompt("Please enter the number of squares per side you want:");
+    clearGrid();
+    populateGrid(value);
+}
+
+function clearGrid() {
+    container.innerHTML = "";
+}
+
+function randomRGB() {
+    let ranNum = Math.floor(Math.random() * 255.999);
+    let ranNum2 = Math.floor(Math.random() * 255.999);
+    let ranNum3 = Math.floor(Math.random() * 255.999);
+    return "rgb(" + ranNum.toString() + "," + ranNum2.toString() + "," + ranNum3.toString() + ")";
+}
+
+container.addEventListener("mouseover", (event) => {
+    let colour = randomRGB();
+    console.log(colour);
+    event.target.style.backgroundColor = colour;
+    
+});
+
 
 populateGrid(16);
